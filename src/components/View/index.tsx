84d3141bdfadf381e29313874d7b454f0d5118ca
Henry Burgess <henryjburg@gmail.com>
2025-01-07 16:07:03 -0600

    Partial keyboard interaction with plugin

--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -1,11 +1,11 @@
 // React and Grommet
 import React, { ReactElement, useState } from "react";
-import ReactDOM from "react-dom";
 import {
   Box,
   Button,
   Grommet,
   Heading,
+  Keyboard,
   Layer,
   RadioButtonGroup,
   Select,
@@ -13,8 +13,7 @@ import {
 } from "grommet";
 import { Next } from "grommet-icons";
 
-import { useEventListener, useTimeout } from "usehooks-ts";
-import Key from "../Key";
+import { useTimeout } from "usehooks-ts";
 
 // Custom theme
 import { Theme } from "../../theme";
@@ -33,9 +32,7 @@ const View = (props: ViewProps): ReactElement => {
   // Participant selection and correctness
   const [selection, setSelection] = useState("");
   const [selectionCorrect, setSelectionCorrect] = useState(false);
-  const [correctResponse] = useState(
-    props.responses.filter((r) => r.correct === true)[0].value
-  );
+  const [selectedResponseIndex, setSelectedResponseIndex] = useState(0);
 
   // Label of the continue button underneath the responses
   const [buttonLabel, setButtonLabel] = useState("Continue");
@@ -47,6 +44,15 @@ const View = (props: ViewProps): ReactElement => {
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [showFeedback, setShowFeedback] = useState(false);
 
+  // Selection state if using alternate input scheme
+  const [selectedElementIndex, setSelectedElementIndex] = useState(0);
+  const [elementFocused, setElementFocused] = useState(false);
+
+  const useAlternateInput =
+    props.input_schema.select !== null &&
+    props.input_schema.next !== null &&
+    props.input_schema.previous !== null;
+
   /**
    * Either prompt confirmation or check the response depending on
    * the 'continue' configuration
@@ -54,7 +60,7 @@ const View = (props: ViewProps): ReactElement => {
    */
   const continueTrial = (): void => {
     // Show the confirmation message if it enabled and has not been shown
-    if (props.continue.confirm && showConfirmation === false) {
+    if (props.confirm_continue === true && showConfirmation === false) {
       setButtonLabel("Confirm?");
       setShowConfirmation(true);
       return;
@@ -69,16 +75,13 @@ const View = (props: ViewProps): ReactElement => {
    * @param {string} finalSelection the selection made by the participant
    */
   const checkResponse = (finalSelection: string) => {
-    setSelectionCorrect(finalSelection === correctResponse);
+    setSelectionCorrect(props.responses.indexOf(finalSelection) === props.correct);
   };
 
   /**
    * End the trial, calling the callback function
    */
   const endTrial = () => {
-    // Unbind all keyboard handlers
-    removeEventListener("keyup", keyboardHandler);
-
     // Call the callback function
     props.callback({
       attentionSelection: selection,
@@ -90,34 +93,45 @@ const View = (props: ViewProps): ReactElement => {
   /**
    * Generic keyboard handler, filters input and progresses trial
    * as required
-   * @param event the keyboard event to be processed
+   * @param {React.KeyboardEvent<HTMLElement>} event the keyboard event to be processed
    * @return {void}
    */
-  const keyboardHandler = (event: KeyboardEvent): void => {
+  const keyboardHandler = (event: React.KeyboardEvent<HTMLElement>): void => {
     // Ignore all input if the timeout has not expired
     if (timeoutExpired === false) return;
 
     // Ignore all input if keyboard input is not enabled
-    if (props.style === "default" || props.continue.key === null) return;
+    if (props.style === "default" || props.input_schema === null || event.repeat) return;
 
-    // Get key information
-    const key = event.key;
+    const key = event.key.toString();
 
-    // Normalize key strings for comparison
-    const submitKey = props.continue.key.toLocaleLowerCase();
-
-    if (key === submitKey && showFeedback === true && selection !== "") {
-      // Confirmation shown, but trial not ended
-      endTrial();
-    } else if (key === submitKey && selection !== "") {
-      // Selection has been made, but confirmation not shown
-      continueTrial();
-    } else if (showFeedback === false) {
-      // Select the corresponding option
-      for (const response of props.responses) {
-        if (response.key.toLocaleLowerCase() === key) {
-          setSelection(response.value);
+    if (key === props.input_schema.next || key === props.input_schema.previous) {
+      if (elementFocused === false) {
+        // Elements not yet focused, moving between UI elements
+        setSelectedElementIndex(selectedElementIndex === 0 ? 1 : 0);
+      } else if (selectedElementIndex === 0) {
+        // Option within radio buttons
+        let updatedSelectedResponseIndex = selectedResponseIndex;
+        if (selection !== "" && key === props.input_schema.next) {
+          updatedSelectedResponseIndex = selectedResponseIndex + 1 < props.responses.length ? selectedResponseIndex + 1 : props.responses.length - 1;
+        } else if (selection !== "" && key === props.input_schema.previous) {
+          updatedSelectedResponseIndex = selectedResponseIndex - 1 >= 0 ? selectedResponseIndex - 1 : 0;
+        } else {
+          updatedSelectedResponseIndex = 0;
         }
+
+        // Update selection state
+        setSelectedResponseIndex(updatedSelectedResponseIndex);
+        setSelection(props.responses[updatedSelectedResponseIndex]);
+      }
+    } else if (key === props.input_schema.select) {
+      if (selectedElementIndex === 1) {
+        // Continue button
+        if (selection !== "" && timeoutExpired) {
+          continueTrial();
+        }
+      } else {
+        setElementFocused(!elementFocused);
       }
     }
   };
@@ -125,210 +139,171 @@ const View = (props: ViewProps): ReactElement => {
   // Set a timeout to block input
   useTimeout(() => setTimeoutExpired(true), props.input_timeout);
 
-  // Setup an event listener for the 'keyup' event
-  useEventListener("keyup", keyboardHandler);
-
   // Record start time
   const startTime = performance.now();
 
   // Return component
   return (
     <Grommet theme={Theme}>
-      <Box direction="column" align="center" justify="center" gap="small" fill>
-        <Heading>{props.prompt}</Heading>
-        <Box
-          pad="medium"
-          margin={{ top: "xsmall", bottom: "large" }}
-          align="center"
-          justify="center"
-          fill="horizontal"
-          border={{
-            color: "light-4",
-            size: "medium",
-            side: "all",
-          }}
-          round
-        >
-          {/* 'Select' component or 'RadioButtonGroup' component depending on configuration */}
-          {props.style === "default" ? (
-            // 'Select' component
-            <Box width={{ max: "large" }} fill>
-              <Select
-                name="responses"
-                options={props.responses.map((r) => {
-                  return r.value;
-                })}
-                size="medium"
-                width="xlarge"
-                value={selection}
-                onChange={({ option }) => setSelection(option)}
-                placeholder={"Select"}
-                disabled={!timeoutExpired}
-              />
-            </Box>
-          ) : (
-            props.continue.key !== null ?
-            // 'RadioButtonGroup' component with radio selectors hidden, keyboard input only
-            <RadioButtonGroup
-              name="responses"
-              width={{ min: "small", max: "2xl" }}
-              options={props.responses.map((r) => {
-                return r.value;
-              })}
-              value={selection}
-              onChange={(event) => {
-                // Update selection only if input is enabled
-                if (timeoutExpired) setSelection(event.target.value);
-              }}
-              disabled={!timeoutExpired}
-            >
-              {(option: string) => {
-                // Find the corresponding key
-                let key = null;
-                for (const response of props.responses) {
-                  if (option === response.value) {
-                    key = response.key;
-                    break;
-                  }
-                }
-                return (
-                    <Box
-                      direction="row"
-                      justify="center"
-                      align="center"
-                      gap="medium"
-                      animation="fadeIn"
-                    >
-                      {/* Display the keyboard key if specified */}
-                      {key !== null ? <Key value={key} pressed={selection === option} /> : null}
-                      <Text size="xlarge">{option}</Text>
-                    </Box>
-                  )
-                }}
-            </RadioButtonGroup>
-            :
-            // 'RadioButtonGroup' component with radio selectors visible
-            <RadioButtonGroup
-              name="responses"
-              width={{ min: "small", max: "2xl" }}
-              options={props.responses.map((r) => {
-                return {
-                  id: r.value,
-                  label: (
-                    <Box
-                      direction="row"
-                      justify="center"
-                      align="center"
-                      gap="medium"
-                      animation="fadeIn"
-                    >
-                      <Text size="xlarge">{r.value}</Text>
-                    </Box>
-                  ),
-                  value: r.value,
-                };
-              })}
-              value={selection}
-              onChange={(event) => {
-                // Update selection only if input is enabled
-                if (timeoutExpired) setSelection(event.target.value);
-              }}
-              disabled={!timeoutExpired}
-            />
-          )}
-        </Box>
-
-        {/* 'Continue' button */}
-        <Box direction="row" gap="medium">
-          {props.continue.key !== null ? (
-            <Key
-              value={props.continue.key}
-              disabled={selection === "" || !timeoutExpired}
-            />
-          ) : null}
-          <Button
-            size="large"
-            label={buttonLabel}
-            disabled={selection === "" || !timeoutExpired}
-            onClick={() => continueTrial()}
-            icon={<Next />}
-            color={showConfirmation === false ? "light-4" : "status-warning"}
-            primary
-            reverse
-          />
-        </Box>
-
-        {/* Feedback dialog */}
-        {showFeedback && (
-          <Layer>
-            <Box
-              direction="column"
-              pad="medium"
-              gap="medium"
-              align="center"
-              fill
-            >
-              <Box>
-                {/* Top-level feedback heading */}
-                <Heading
-                  margin="small"
-                  color={selectionCorrect === true ? "green" : "red"}
-                >
-                  {selectionCorrect === true ? "Correct!" : "Incorrect."}
-                </Heading>
-              </Box>
-
-              {/* Prompt */}
-              <Box>
-                <Text size="xlarge">{props.prompt}</Text>
-              </Box>
-
-              {/* Participant selection and correct response (if incorrect) */}
-              <Box align="left" gap="small" fill>
-                <Box>
-                  <Text size="xlarge">
-                    <strong>You selected:</strong> "{selection}"
-                  </Text>
-                </Box>
-                {selectionCorrect === false ? (
-                  <Box>
-                    <Text size="xlarge">
-                      <strong>Correct response:</strong> "{correctResponse}"
-                    </Text>
-                  </Box>
-                ) : null}
-
-                {/* Feedback for response */}
-                <Box>
-                  <Text size="xlarge">
-                    <strong>Feedback:</strong>{" "}
-                    {selectionCorrect === true
-                      ? props.feedback.correct
-                      : props.feedback.incorrect}
-                  </Text>
-                </Box>
-              </Box>
-
-              {/* Continue button */}
-              <Box direction="row" gap="medium">
-                {props.continue.key !== null ? (
-                  <Key value={props.continue.key} />
-                ) : null}
-                <Button
-                  size="large"
-                  label="Continue"
-                  disabled={selection === ""}
-                  onClick={() => endTrial()}
-                  icon={<Next />}
-                  color="light-4"
-                  primary
-                  reverse
+      <Keyboard onKeyDown={keyboardHandler} target={"document"}>
+        <Box direction={"column"} align={"center"} justify={"center"} gap={"small"} fill>
+          <Heading size={"small"} level={"2"}>{props.prompt}</Heading>
+          <Box
+            pad={"medium"}
+            margin={{ top: "xsmall", bottom: "large" }}
+            align={"center"}
+            justify={"center"}
+            fill={"horizontal"}
+            border={{
+              color: useAlternateInput && selectedElementIndex === 0 && !elementFocused ? "lightgray" : "transparent",
+              size: "large",
+            }}
+            round
+          >
+            {/* 'Select' component or 'RadioButtonGroup' component depending on configuration */}
+            {props.style === "default" ? (
+              // 'Select' component
+              <Box width={{ max: "md" }} fill>
+                <Select
+                  name={"responses"}
+                  options={props.responses.map((r) => {
+                    return r;
+                  })}
+                  size={"medium"}
+                  width={"large"}
+                  value={selection}
+                  onChange={({ option }) => setSelection(option)}
+                  placeholder={"Select"}
+                  disabled={!timeoutExpired}
                 />
               </Box>
-            </Box>
-          </Layer>
-        )}
-      </Box>
+            ) : (
+              // 'RadioButtonGroup' component with radio selectors visible
+              <RadioButtonGroup
+                name="responses"
+                width={{ min: "small", max: "2xl" }}
+                options={props.responses.map((r) => {
+                  return {
+                    id: r,
+                    label: (
+                      <Box
+                        direction={"row"}
+                        justify={"center"}
+                        align={"center"}
+                        gap={"medium"}
+                        animation={"fadeIn"}
+                      >
+                        <Text size={"medium"}>{r}</Text>
+                      </Box>
+                    ),
+                    value: r,
+                  };
+                })}
+                value={selection}
+                onChange={(event) => {
+                  // Update selection only if input is enabled
+                  if (timeoutExpired) setSelection(event.target.value);
+                }}
+                disabled={!timeoutExpired}
+              />
+            )}
+          </Box>
+
+          {/* 'Continue' button */}
+          <Box
+            direction={"row"}
+            gap={"medium"}
+            pad={"none"}
+            border={{
+              color: useAlternateInput && selectedElementIndex === 1 ? "lightgray" : "transparent",
+              size: "large",
+            }}
+            style={{ borderRadius: "32px" }}
+            round
+          >
+            <Button
+              label={buttonLabel}
+              disabled={selection === "" || !timeoutExpired}
+              onClick={() => continueTrial()}
+              icon={<Next />}
+              color={showConfirmation === false ? "brand" : "status-warning"}
+              primary
+              reverse
+            />
+          </Box>
+
+          {/* Feedback dialog */}
+          {showFeedback && (
+            <Layer>
+              <Box
+                direction={"column"}
+                pad={"medium"}
+                gap={"medium"}
+                align={"center"}
+                fill
+              >
+                <Box>
+                  {/* Top-level feedback heading */}
+                  <Heading
+                    level={"2"}
+                    margin={"small"}
+                    color={selectionCorrect === true ? "green" : "red"}
+                  >
+                    {selectionCorrect === true ? "Correct!" : "Incorrect."}
+                  </Heading>
+                </Box>
+
+                {/* Prompt */}
+                <Box>
+                  <Text size={"large"}>{props.prompt}</Text>
+                </Box>
+
+                {/* Participant selection and correct response (if incorrect) */}
+                <Box align={"left"} gap={"small"} fill>
+                  <Box>
+                    <Text size={"large"}>
+                      <strong>You selected:</strong> "{selection}"
+                    </Text>
+                  </Box>
+                  {selectionCorrect === false ? (
+                    <Box>
+                      <Text size={"large"}>
+                        <strong>Correct response:</strong> "{props.responses[props.correct]}"
+                      </Text>
+                    </Box>
+                  ) : null}
+
+                  {/* Feedback for response */}
+                  <Box>
+                    <Text size={"large"}>
+                      <strong>Feedback:</strong>{" "}
+                      {selectionCorrect === true
+                        ? props.feedback.correct
+                        : props.feedback.incorrect}
+                    </Text>
+                  </Box>
+                </Box>
+
+                {/* Continue button */}
+                <Box
+                  direction={"row"}
+                  gap={"medium"}
+                >
+                  <Button
+                    label={"Continue"}
+                    disabled={selection === ""}
+                    onClick={() => endTrial()}
+                    icon={<Next />}
+                    color={"light-4"}
+                    primary
+                    reverse
+                  />
+                </Box>
+              </Box>
+            </Layer>
+          )}
+        </Box>
+      </Keyboard>
     </Grommet>
   );
 };
